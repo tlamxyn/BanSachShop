823d393e4534875b5c0527ba82f515a22a5080a8
Tran Lam <tlamxyn@gmail.com>
2022-01-16 14:06:30 +0700

    cập nhật OrderController

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -4,12 +4,75 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\hoadon;
 
 class OrderController extends Controller
 {
     //
     public function index()
     {
-        return view('admin.orders.index');
+        //Tạo 1 biến chứa danh sách hóa đơn
+        $danhsach_hoadon = hoadon::all();
+        return view('admin.orders.index', compact('danhsach_hoadon'));
+    }
+
+    public function create(Request $request)
+    {
+        $hoadon = new hoadon();
+        $hoadon->MaHD = $request->input('MaHD');
+        $hoadon->MaKH = $request->input('MaKH');
+        $hoadon->Tongtien = $request->input('Tongtien');
+        $hoadon->Trangthai = $request->input('Trangthai');
+        $hoadon->GiamGia = $request->input('GiamGia');
+        $hoadon->TenKH = $request->input('TenKH');
+        $hoadon->Diachigiaohang = $request->input('Diachigiaohang');
+        $hoadon->Phuongthucthanhtoan = $request->input('Phuongthucthanhtoan');
+        $hoadon->Sodienthoai = $request->input('Sodienthoai');
+        $hoadon->Ngaylap = $request->input('Ngaylap');
+        $hoadon->MaNV = $request->input('MaNV');
+        $hoadon->save();
+        return redirect()->back();
+    }
+
+    public function detail($mahd)
+    {
+        if (hoadon::where('MaHD', $mahd)->exists()) {
+            $hoadon = hoadon::find($mahd);
+            return view('admin.orders.detail', compact('hoadon'));
+        }
+        else
+        {
+            return abort(404);
+        }
+    }
+
+    public function update(Request $request, $mahd)
+    {
+        $hoadon = hoadon::find($mahd);
+        $hoadon->MaKH = $request->input('MaKH');
+        $hoadon->Tongtien = $request->input('TongTien');
+        $hoadon->Trangthai = $request->input('Trangthai');
+        $hoadon->GiamGia = $request->input('GiamGia');
+        $hoadon->TenKH = $request->input('TenKH');
+        $hoadon->Diachigiaohang = $request->input('Diachigiaohang');
+        $hoadon->Phuongthucthanhtoan = $request->input('Phuongthucthanhtoan');
+        $hoadon->Sodienthoai = $request->input('Sodienthoai');
+        $hoadon->Ngaylap = $request->input('Ngaylap');
+        $hoadon->MaNV = $request->input('MaNV');
+        $hoadon->update();
+    }
+
+    public function delete($mahd)
+    {
+        if(hoadon::where('MaHD', $mahd)->exists())
+        {
+            $hoadon = hoadon::find($mahd);
+            foreach($hoadon->chitiethd as $cthd)
+            {
+                $cthd->delete();
+            }
+            $hoadon->delete();
+            return redirect()->back();
+        }
     }
 }
