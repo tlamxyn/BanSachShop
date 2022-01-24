1620ed9de37c660c8ab640f8f3c552516efdc418
Leanhvu-0306191488 <62364561+Leanhvu-0306191488@users.noreply.github.com>
2022-01-24 19:58:15 +0700

    add layout

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace App\Http\Controllers\home;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class homeController extends Controller
+class HomeController extends Controller
 {
-    //
     public function index()
     {
         return view('index');
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
-Route::get('/home', 'index');
+Route::get('/','App\Http\Controllers\HomeController@index')->name('index');
