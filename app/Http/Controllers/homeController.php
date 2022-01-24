da33d5942c4ab05a8d49be9d8c4abcce4eea2245
Leanhvu-0306191488 <62364561+Leanhvu-0306191488@users.noreply.github.com>
2022-01-24 19:46:45 +0700

    add

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\home;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class homeController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-// Route::get('/home', 'index');
+Route::get('/home', 'index');
