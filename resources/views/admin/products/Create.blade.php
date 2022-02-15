b992604c98852de5ba0bc50dc4dc3b212a1c3055
Leanhvu-0306191488 <62364561+Leanhvu-0306191488@users.noreply.github.com>
2022-02-15 07:33:00 +0700

    Update Create.blade.php

--- resources/views/admin/products/Create.blade.php
+++ resources/views/admin/products/Create.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('title') 
 @section('content')
+@php
+    $data = App\Models\theloai::all();  
+@endphp
 <div class="container">
     <div class="row">
         <div class="col-md-4">
@@ -10,8 +13,17 @@
         
                     <label  for="my-input" class="control-label" style="margin-top: 10px"> Tên Sản Phẩm </label>
                     <input  id="my-input" required class="form-control" type="text" name="Ten">
+                    
                     <label  for="my-input" class="control-label" style="margin-top: 10px">Thể Loại</label>
+                    {{-- <input  id="my-input" required class="form-control" type="text" name="Theloai"> --}}
+                    <select name="Theloai" id="my-input" required class="form-control">
+                        @foreach ($data as $tl )
+                        <option value="{{  $tl->MaTL }}">{{  $tl->Ten }}</option>
+                        @endforeach
+                        
+                      </select>
 
+                    <label  for="my-input" class="control-label" style="margin-top: 10px">Mô tả</label>
                     <input  id="my-input" required class="form-control" type="text" name="Mota">
             
                 
@@ -20,6 +32,7 @@
             
                 
                     <label  for="my-input" class="control-label" style="margin-top: 10px">Nhà Xuất Bản</label>
+                    <input  id="my-input" required class="form-control" type="text" name="NxB">           
                 
                     <label  for="my-input" class="control-label" style="margin-top: 10px">Trạng Thái</label>
                     <input  id="my-input" required class="form-control" type="text" name="Trangthai">
@@ -27,6 +40,7 @@
                 
                     <label  for="my-input" class="control-label" style="margin-top: 10px">Hình Ảnh</label>
                     <input  id="my-input" required class="form-control" type="text" name="Hinhanh">
+                    <input  id="my-input" required class="form-control" type="file" name="Hinhanh">
             
                 <a href="{{ route('admin.products.index') }}" type="submit" class="btn btn-primary">Back To List</a>
                 <button name="submit" id="" class="btn btn-primary" type="submit">Thêm</button>
