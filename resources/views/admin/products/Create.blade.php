b999289f48e0b8b8c586132de06108b11c79bad3
Leanhvu-0306191488 <62364561+Leanhvu-0306191488@users.noreply.github.com>
2022-02-15 07:32:34 +0700

    a
    a

--- resources/views/admin/products/Create.blade.php
+++ resources/views/admin/products/Create.blade.php
@@ -10,11 +10,8 @@
         
                     <label  for="my-input" class="control-label" style="margin-top: 10px"> Tên Sản Phẩm </label>
                     <input  id="my-input" required class="form-control" type="text" name="Ten">
-                
                     <label  for="my-input" class="control-label" style="margin-top: 10px">Thể Loại</label>
-                    <input  id="my-input" required class="form-control" type="text" name="Theloai">
 
-                    <label  for="my-input" class="control-label" style="margin-top: 10px">Moo tar</label>
                     <input  id="my-input" required class="form-control" type="text" name="Mota">
             
                 
@@ -23,16 +20,6 @@
             
                 
                     <label  for="my-input" class="control-label" style="margin-top: 10px">Nhà Xuất Bản</label>
-                    <input  id="my-input" required class="form-control" type="text" name="NxB">
-                
-                
-                    <label  for="my-input" class="control-label" style="margin-top: 10px">Số Lượng</label>
-                    <input  id="my-input" required class="form-control" type="text" name="SLtonkho">
-            
-                
-                    <label  for="my-input" class="control-label" style="margin-top: 10px">Giá Bán</label>
-                    <input  id="my-input" required class="form-control" type="text" name="Giaban">
-            
                 
                     <label  for="my-input" class="control-label" style="margin-top: 10px">Trạng Thái</label>
                     <input  id="my-input" required class="form-control" type="text" name="Trangthai">
